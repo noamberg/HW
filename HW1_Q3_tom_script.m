3d41b19753f718db561fc9d24d79059e8d4ebe13
Tomlevron <54799120+Tomlevron@users.noreply.github.com>
2020-11-18 15:01:15 +0200

    for noam

--- HW1_Q3_tom_script.m
+++ HW1_Q3_tom_script.m
@@ -3,31 +3,38 @@
 clc;clear;close all;
 l_fixed = 632.8; % in nm
 n_air = 1; n_water = 1.33;
-d_Ag = 50;
-d = [NaN,d_Ag,inf];
-
+d_Ag = [35,40,45,50,55];
+d_35 = [NaN,d_Ag(1),inf];
+d_40 = [NaN,d_Ag(2),inf];
+d_45 = [NaN,d_Ag(3),inf];
+d_50 = [NaN,d_Ag(4),inf];
+d_55 = [NaN,d_Ag(5),inf];
+polarization_te = 0; polarization_tm = 1; % Tm = P ; Te = s
 conv_coef=1239.8424; %Conversion coefficent for the eV units
 
 n_p = Prism_SF11(l_fixed);
-
 n_Ag = Ag_ref_index(l_fixed);
+n = [n_p, n_Ag, n_air];
 
 theta=30:0.1:60;
-n = [n_p, n_Ag, n_air];
-polarization_te = 0; polarization_tm = 1; % Tm = P ; Te = s
+
 for i=1:length(theta)
-    [r1(i),t1(i),R1(i),T1(i),A1(i)]= ABELES(l_fixed,d,n,deg2rad(theta(i)),polarization_te);
-    [r2(i),t2(i),R2(i),T2(i),A2(i)]= ABELES(l_fixed,d,n,deg2rad(theta(i)),polarization_tm);
+    [r_35(i),t_35(i),R_35(i),T_35(i),A_35(i)]= ABELES(l_fixed,d_35,n,deg2rad(theta(i)),polarization_tm);
+    [r_40(i),t_40(i),R_40(i),T_40(i),A_40(i)]= ABELES(l_fixed,d_40,n,deg2rad(theta(i)),polarization_tm);
+    [r_45(i),t_45(i),R_45(i),T_45(i),A_45(i)]= ABELES(l_fixed,d_45,n,deg2rad(theta(i)),polarization_tm);
+    [r_50(i),t_50(i),R_50(i),T_50(i),A_50(i)]= ABELES(l_fixed,d_50,n,deg2rad(theta(i)),polarization_tm);
+    [r_55(i),t_55(i),R_55(i),T_55(i),A_55(i)]= ABELES(l_fixed,d_55,n,deg2rad(theta(i)),polarization_tm);
 end
 figure;
-plot(theta,R1,theta,R2)
+plot(theta,R_35,theta,R_40,theta,R_45,theta,R_50,theta,R_55,'linewidth',2)
 % ylim([0,1])
-% xlim([40 60])
+ xlim([30 44])
 grid on;
 title('Reflection as function of incident angle at fixed wavelength of 633 nm')
 xlabel('\Theta [deg]')
 ylabel('Reflectivity')
-legend('R-TE','R-TM')
+legend('R-TM Ag thickness of 35 nm','R-TM Ag thickness of 40 nm','R-TM Ag thickness of 45 nm',...
+    'R-TM Ag thickness of 50 nm', 'R-TM Ag thickness of 55 nm')
 
 %% Sensitiviy check
 n3 = [n_p, n_Ag, 1.33+0.015];
